fed88f5d061b6569b06105169fabe1caf75a1448
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-04-28 09:12:05 +0100

    Add function for uppercase route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { sayHello } = require('./lib/strings');
+const { sayHello, uppercase } = require('./lib/strings');
 
 const app = express();
 
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.string) });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
 });
 
 module.exports = app;
