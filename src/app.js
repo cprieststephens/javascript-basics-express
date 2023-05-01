fad9d123edbf5f895f341ef76d4b80683d335c08
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-05-01 13:26:41 +0100

    Add functions for element-at-index, to-string, append, starts-with-vowel and remove-element routes

--- src/app.js
+++ src/app.js
@@ -12,6 +12,14 @@ const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
 const { negate, truthiness, isOdd, startsWith } = require('./lib/booleans');
 
+const {
+  getNthElement,
+  arrayToCSVString,
+  addToArray2,
+  elementsStartingWithAVowel,
+  removeNthElement2,
+} = require('./lib/arrays');
+
 const app = express();
 
 app.use(express.json());
@@ -124,7 +132,33 @@ app.get('/booleans/:string/starts-with/:char', (req, res) => {
   if (req.params.char.length > 1) {
     res.status(400).json({ error: 'Parameter "character" must be a single character.' });
   } else {
-    res.status(200).json({ result: req.params.string.startsWith(req.params.char) });
+    res.status(200).json({ result: startsWith(req.params.char, req.params.string) });
+  }
+});
+
+// Arrays
+
+app.post('/arrays/element-at-index/:index', (req, res) => {
+  res.status(200).json({ result: getNthElement(req.params.index, req.body.array) });
+});
+
+app.post('/arrays/to-string', (req, res) => {
+  res.status(200).json({ result: arrayToCSVString(req.body.array) });
+});
+
+app.post('/arrays/append', (req, res) => {
+  res.status(200).json({ result: addToArray2(req.body.value, req.body.array) });
+});
+
+app.post('/arrays/starts-with-vowel', (req, res) => {
+  res.status(200).json({ result: elementsStartingWithAVowel(req.body.array) });
+});
+
+app.post('/arrays/remove-element', (req, res) => {
+  if (req.query.index) {
+    res.status(200).json({ result: removeNthElement2(req.query.index, req.body.array) });
+  } else {
+    res.status(200).json({ result: removeNthElement2(0, req.body.array) });
   }
 });
 
